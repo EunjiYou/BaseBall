741133c4ecb5b9e4d820d41ac2fe4d861c9d4cdc
김태현 <cplkimth@naver.com>
2018-07-20 11:59:24 +0900

    .

--- BaseBall/Program.cs
+++ BaseBall/Program.cs
@@ -14,34 +14,36 @@ namespace BaseBall
             ShowWelcomeMessage();
 
             // 랜덤한 정답을 생성한다.
-            int[] answers = GenerateAnswers();
-            int[] guesses = new int[3];
+            Answer answer = new Answer();
+            answer.Generate();
 
-            string[] inputMessages = { "> 첫 번째 숫자를 입력하세요.", "> 두 번째 숫자를 입력하세요.", "> 세 번째 숫자를 입력하세요." };
+            Guess guess = new Guess();
 
             while (true)
             {
                 // 추측을 입력받는다
-                InputGuesses(guesses, inputMessages);
+                guess.Input();
 
                 // 입력받은 추측을 출력한다.
-                ShowGuesses(guesses);
+                //ShowGuesses(guesses);
+                guess.Show();
 
                 // 추측의 유효성을 검사한다.
-                if (IsInvalidGuesses(guesses))
+                if (guess.IsInvalid())
                 {
                     Console.WriteLine("같은 숫자를 입력하면 안 됩니다.");
                     continue;
                 }
 
                 // 결과를 계산한다.
-                Result result = CalculateResult(answers, guesses);
+                Result result = new Result();
+                result.Calculate(answer, guess);
 
                 // 결과를 출력한다.
-                ShowResult(result);
+                result.Show();
 
                 // 정답이면 게임을 끝낸다
-                if (result.Strike == 3)
+                if (result.IsFinished())
                 {
                     Console.WriteLine("정답입니다!");
                     break;
